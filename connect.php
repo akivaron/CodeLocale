5eff3253c58c4143f0d3dd90893bf4459f4c685b
earthyT <31322069+earthyT@users.noreply.github.com>
2017-12-19 11:02:58 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+	$mysql_hostname = "localhost";
+	$mysql_user = "inolas-irawit";
+	$mysql_password = "qwerty";
+	$mysql_database = "codelocale";
+$con = mysqli_connect($mysql_hostname,$mysql_user,$mysql_password,$mysql_database);
+  if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+   ?>
